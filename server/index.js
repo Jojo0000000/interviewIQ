c4867c1fb209aefdebc4e958ac05608ce6b75397
Jojo0000000 <bishtjyoti664@gmail.com>
2026-06-15 22:05:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin:"https://interviewiq-clientt.onrender.com",
     credentials:true
 }))
 
